bedccd82b540fd0efcaf59cb99fc7b4dfb39d017
Karthick Pranesh GSD <146869960+KarthickPraneshGSD@users.noreply.github.com>
2026-01-12 18:10:44 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,10 @@
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBIZtoVNy88BFnBrJP74jPdZAv01JvfqsU",
   authDomain: "smart-inventory-rack.firebaseapp.com",
   projectId: "smart-inventory-rack",
-  storageBucket: "smart-inventory-rack.appspot.com",
-  messagingSenderId: "1081356245240",
-  appId: "1:1081356245240:web:80173a12a979bb6ce2030d"
 };
 
-// Initialize Firebase (COMPAT MODE)
 firebase.initializeApp(firebaseConfig);
-
-// Initialize Firestore
 const db = firebase.firestore();
 
-// Debug check
-console.log("Firebase connected", db);
+console.log("🔥 Firebase initialized");
